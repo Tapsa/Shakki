1eed77f75b9cc4d9ff2ec29c4c4fce0e41606462
Mikko <mikko.partonen@metropolia.fi>
2015-12-02 13:19:19 +0200

    Pieces centered. New app icons.

--- app/src/test/java/tapsa/shakki/PositionTest.java
+++ app/src/test/java/tapsa/shakki/PositionTest.java
@@ -4,7 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Tapsa on 13.11.2015.
